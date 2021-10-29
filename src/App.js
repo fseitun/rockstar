c7c22c521093d815bd0c4961ee062a6a25ae16f8
fseitun <fseitun@gmail.com>
2021-10-29 14:21:46 -0300

    Best movies page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Routes, Route } from 'react-router';
+import { BestMovies } from './Components/BestMovies.js';
 import './App.css';
 
-function App() {
-  return 'hi';
-}
+const apiKey = '92744beada41e5a4bd0cc80048ae44df';
 
-export default App;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<BestMovies apiKey={apiKey} />} />
+      <Route path="tasks" element={<BestMovies />} />
+      <Route path="about" element={<BestMovies />} />
+    </Routes>
+  );
+}
